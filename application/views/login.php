b032747567cf51a583ee009ff8892ed655beac41
Ryan Andonian <randonia@ucsc.edu>
2011-11-10 12:57:32 -0800

    Fixed some login stuff. Still haven't gotten it creating a user, but I fixed the "you dun goofed" response.

--- application/views/login.php
+++ application/views/login.php
@@ -3,12 +3,13 @@
 <h1>Registration Form</h1>
 <div id="body">
    <p><? if(isset($errorcode)){ echo $errorcode;}?>
-   <form name="registrationForm" action="commitForm" method="post">
+ <form name="registrationForm" action="login/commitForm/" method="post">
 	Username: <input type="text" name="username"><br>
-	Password: <input type="text" name="password1"><br>
-	Confirm Password: <input type="text" name="password2"><br>
+	Password: <input type="password" name="password1"><br>
+	Confirm Password: <input type="password" name="password2"><br>
 	E-mail: <input type="text" name="email"><br>
-	<input type="submit" value="Register!"><br>
+	<input type="submit" value="Register!">
+    <input type="reset"><br>
    </form>
    </p>
 </div>
