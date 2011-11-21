e38abd65b3bf3322d8e576d60dc9fc361a693f86
Ryan Andonian <randonia@ucsc.edu>
2011-11-20 20:18:04 -0800

    Fixed the registered.php "go back via this link" link

--- application/views/registered.php
+++ application/views/registered.php
@@ -2,7 +2,7 @@
 <?= closeHeader()?>
 <h1>User Registration</h1>
 <div id="body">
-       <p>Thank you for registering, <?= $this->session->userdata('username')?>!. You can go back to the main page via this <a href="<?site_url()?>">link</a></p>
+       <p>Thank you for registering, <?= $this->session->userdata('username')?>!. You can go back to the main page via this <a href="<?=site_url()?>">link</a></p>
    
 </div>
 <?= generateFooter()?>
