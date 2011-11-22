8141d80b0ddf7bf1a925383f395065c3e7776370
Ryan Andonian <randonia@ucsc.edu>
2011-11-21 22:33:30 -0800

    Doin some home stuffs

--- application/views/home.php
+++ application/views/home.php
@@ -12,22 +12,20 @@
 ?></div>
 </div></h1>
     <div id="body">
-    <p>This page/project still under development. <a href="http://cmps183-fall2011.soe.ucsc.edu/doku.php?id=carpoolamajig_project" class="inText">Here</a> is our class project page</p>
-    <p>Pages we have up: <br>
-       <ul>
-       <li><a class="inNav" href="index.php/register">Registration Page</a></li>
-       <li><a class="inNav" href="index.php/login">Login Page</a></li>
-       <li><a class="inNav" href="index.php/events">Events Page</a></li>
-       <li><a class="inNav" href="index.php/events/createEvent">Create an Event page</a></li>
-       </ul>
-    </p>
-        <a href="http://www.youtube.com/watch?v=izGwDsrQ1eQ" class="inNav">In the mean time, enjoy this.</a>
-    </p>
-
+	 <p class ="nav">
+		<a class="inNav" href="index.php/events">Events</a><br>
+		<a class="inNav" href="index.php/routes">Routes</a><br>
+		<a class="inNav" href="index.php/users">Users</a><br>
+		<a class="inNav" href="index.php/search">Search</a><br>
+	</p>
+    <p>This page/project still under development. <a href="http://cmps183-fall2011.soe.ucsc.edu/doku.php?id=carpoolamajig_project" class="inText">Here</a> is our class project page</p><br><br><br><br><br><br>
+	<p class="content">
+	</p>
     <p class="footer">
          <? if(isset($dateMod)){echo "This page was last updated on $dateMod";}?> 
-We are using <a href="http://codeigniter.com/" class="inNav">CodeIgniter</a> for this project. w00tcakes.</p>
+We are using <a href="http://codeigniter.com/" class="inText">CodeIgniter</a> for this project. w00tcakes.</p>
        <? 
        ?>
+	   
     </div>
 <?echo generateFooter()?>
