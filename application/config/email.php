fdbfb87106f9c2e5abd23b2b743662b6ca29fad2
Ryan Andonian <randonia@ucsc.edu>
2011-12-05 00:33:31 -0800

    Added the email configuration file and some examples on how to send emails! Follow the steps in controlls/emailsetup.php::test

---
+++ application/config/email.php
@@ -0,0 +1,12 @@
+<?
+        $config['protocol'] = "smtp";
+        $config['mailpath'] = "/usr/sbin/sendmail";
+        $config['charset'] = 'iso-8859-1';
+        $config['wordwrap'] = TRUE;
+        
+        $config['smtp_host'] = "mail.zambinidirect.com";
+        $config['smtp_user'] = "god@carpoolamajig.com";
+        $config['smtp_pass'] = "thispasswordisverysecure";
+        $config['smtp_port'] = "26";
+        $config['smtp_auth'] = TRUE;
+?>
--- application/controllers/emailsetup.php
+++ application/controllers/emailsetup.php
@@ -2,27 +2,35 @@
 
 class EmailSetup extends CI_Controller{
     function index(){
-        $config['protocol'] = "smtp";
-        $config['mailpath'] = "/usr/sbin/sendmail";
-        $config['charset'] = 'iso-8859-1';
-        $config['wordwrap'] = TRUE;
+        #$config['protocol'] = "smtp";
+        #$config['mailpath'] = "/usr/sbin/sendmail";
+        #$config['charset'] = 'iso-8859-1';
+        #$config['wordwrap'] = TRUE;
         
-        $config['smtp_host'] = "mail.zambinidirect.com";
-        $config['smtp_user'] = "god@carpoolamajig.com";
-        $config['smtp_pass'] = "thispasswordisverysecure";
-        $config['smtp_port'] = "26";
-        $config['smtp_auth'] = TRUE;
-
-       # $this->email->initialize($config);
-        $this->load->library('email',$config);
-        $this->test();
+        #$config['smtp_host'] = "mail.zambinidirect.com";
+        #$config['smtp_user'] = "god@carpoolamajig.com";
+        #$config['smtp_pass'] = "thispasswordisverysecure";
+        #$config['smtp_port'] = "26";
+        #$config['smtp_auth'] = TRUE;
     }
     function test(){
+        return false;
         $this->load->library('email');
-        $this->email->from('god@carpoolamajig.com','Gob');
         $this->email->to('randonia@ucsc.edu');
+        $this->email->from('god@carpoolamajig.com','Gob');
         $this->email->subject('Testies');
         $this->email->message('Testing the email class!');
+        $this->email->send();
+        echo $this->email->print_debugger();
+    }
+
+    function test2(){
+        return false;
+        $this->load->library('email');
+        $this->email->to('randonia@ucsc.edu');
+        $this->email->from('god@carpoolamajig.com','Gob');
+        $this->email->subject('Testies #2');
+        $this->email->message('Testing the email class again!');
         $this->email->send();
         echo $this->email->print_debugger();
     }
